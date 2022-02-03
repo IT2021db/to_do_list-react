6b4d51bc0ab76848a9b0ce433ce69b59643270a2
Dorota Borowska <youcode2021db@gmail.com>
2022-02-03 19:11:49 +0100

    class section__form removed

--- src/Form/index.js
+++ src/Form/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import "./style.css";
 
 const Form = () => (
-    <form className="form section__body">
+    <form className="form">
         <input className="form__input" autoFocus placeholder="Co jest do zrobienia?" />
         <button className="form__button">Dodaj zadanie</button>
     </form>
-
 );
 
 export default Form;
