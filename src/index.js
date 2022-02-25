c94889e7195535f55cecac90b0abfc52f4811c32
Dorota Borowska <youcode2021db@gmail.com>
2022-02-25 13:14:06 +0100

    GlobalStyle component embedded

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./GlobalStyle";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme";
-
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle/>
       <App />
     </ThemeProvider>
   </React.StrictMode>,
