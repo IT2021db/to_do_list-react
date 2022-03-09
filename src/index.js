13d6bbc4c8a2255a4241e6fafc74f4e0a281e105
Dorota Borowska <youcode2021db@gmail.com>
2022-03-09 17:50:26 +0100

    Privider installed

--- src/index.js
+++ src/index.js
@@ -1,58 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from "styled-components";
+import {Provider} from 'react-redux';
 import { GlobalStyle } from "./GlobalStyle";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme";
-import {addTask} from "./features/tasks/tasksSlice";
+import store from './store';
 
-// import { configureStore } from "@reduxjs/toolkit";
 
-// const initialState = {
-//   tasks: [],
-// };
 
-// const taskReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case "addTask":
-//       return {
-//         ...state,
-//         tasks: [
-//           ...state.tasks,
-//           {
-//             content: action.payload,
-//           },
-//         ],
-//       };
-//     default: return state;
-//   }
-// };
-
-// const addTask = content => ({
-//   type: "addTask",
-//   payload: content,
-// });
-
-// const selectTasks = ({ tasks }) => tasks;
-
-// const store = configureStore({ reducer: taskReducer });
-// console.log(selectTasks(store.getState()));
-
-// store.dispatch(addTask("Nauczyć sie dispatch"));
-
-// console.log(store.getState());
-
-// store.dispatch(addTask("Zrobić zadanie 2"));
-
-// console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
