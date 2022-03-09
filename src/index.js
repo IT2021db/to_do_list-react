7121dd25d5d414cc84a8c0c69ada674b25a04c60
Dorota Borowska <youcode2021db@gmail.com>
2022-03-09 17:28:33 +0100

    reducer in index.js- disactived

--- src/index.js
+++ src/index.js
@@ -5,45 +5,47 @@ import { GlobalStyle } from "./GlobalStyle";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { theme } from "./theme";
-import { configureStore } from "@reduxjs/toolkit";
+import {addTask} from "./features/tasks/tasksSlice";
 
-const initialState = {
-  tasks: [],
-};
+// import { configureStore } from "@reduxjs/toolkit";
 
-const taskReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "addTask":
-      return {
-        ...state,
-        tasks: [
-          ...state.tasks,
-          {
-            content: action.payload,
-          },
-        ],
-      };
-    default: return state;
-  }
-};
+// const initialState = {
+//   tasks: [],
+// };
 
-const addTask = content => ({
-  type: "addTask",
-  payload: content,
-});
+// const taskReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "addTask":
+//       return {
+//         ...state,
+//         tasks: [
+//           ...state.tasks,
+//           {
+//             content: action.payload,
+//           },
+//         ],
+//       };
+//     default: return state;
+//   }
+// };
 
-const selectTasks = ({ tasks }) => tasks;
+// const addTask = content => ({
+//   type: "addTask",
+//   payload: content,
+// });
 
-const store = configureStore({ reducer: taskReducer });
-console.log(selectTasks(store.getState()));
+// const selectTasks = ({ tasks }) => tasks;
 
-store.dispatch(addTask("Nauczyć sie dispatch"));
+// const store = configureStore({ reducer: taskReducer });
+// console.log(selectTasks(store.getState()));
 
-console.log(store.getState());
+// store.dispatch(addTask("Nauczyć sie dispatch"));
 
-store.dispatch(addTask("Zrobić zadanie 2"));
+// console.log(store.getState());
 
-console.log(store.getState());
+// store.dispatch(addTask("Zrobić zadanie 2"));
+
+// console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
