957d856f8a5393794367b7e0de3ddc43e387de3c
Dorota Borowska <youcode2021db@gmail.com>
2022-03-09 22:45:37 +0100

    getting state for tasks, hideDone

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,21 +1,27 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Buttons } from "./styled";
+import { selectTasks, toggleHideDone } from "../tasksSlice";
 
-const Button = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-    <div>
-        {tasks.length > 0 && (
-            <>
-                <Buttons onClick={toggleHideDone} >
-                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
-                </Buttons>
-                <Buttons disabled={tasks.every(({ done }) => done)}
-                    onClick={setAllDone}
-                >
-                    Ukończ wszystkie
-                </Buttons>
-            </>
-        )}
-    </div>
-);
+const Button = ({ setAllDone }) => {
+    const { tasks, hideDone } = useSelector(selectTasks);
+    const dispatch = useDispatch();
+    return (
+        <div>
+            {tasks.length > 0 && (
+                <>
+                    <Buttons onClick={() => dispatch(toggleHideDone())} >
+                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                    </Buttons>
+                    <Buttons disabled={tasks.every(({ done }) => done)}
+                        onClick={setAllDone}
+                    >
+                        Ukończ wszystkie
+                    </Buttons>
+                </>
+            )}
+        </div>
+    )
+};
 
 export default Button;
