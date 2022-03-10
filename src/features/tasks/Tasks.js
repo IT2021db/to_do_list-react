b17f2830e55a252f64ea064e0614fca4794440ad
Dorota Borowska <youcode2021db@gmail.com>
2022-03-10 20:02:20 +0100

    useState, useSelector removed

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,17 +1,12 @@
-import React, { useState } from 'react';
-import { useSelector } from "react-redux";
+import React from 'react';
 import Buttons from './Buttons';
 import Form from './Form';
 import TasksList from './TasksList';
 import Section from '../../common/Section';
 import Header from '../../common/Header';
 import Container from '../../common/Container';
-import { selectTasks } from './tasksSlice';
 
 function Tasks() {
-
-  const { tasks } = useSelector(selectTasks);
- 
   return (
     <Container>
       <Header
