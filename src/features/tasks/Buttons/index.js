dd76a91b503824650aa50390aca9e3f350eb91f0
Dorota Borowska <youcode2021db@gmail.com>
2022-03-14 13:20:15 +0100

    exampleTasks: file, button, get-function created

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Buttons } from "./styled";
-import { selectTasks, toggleHideDone, setAllDone } from "../tasksSlice";
+import { selectTasks, toggleHideDone, setAllDone, fetchExampleTasks } from "../tasksSlice";
 
 const Button = () => {
     const { tasks, hideDone } = useSelector(selectTasks);
     const dispatch = useDispatch();
     return (
         <div>
+            <Buttons onClick={() => dispatch(fetchExampleTasks())}>
+                Pobierz przykładowe zadania
+            </Buttons>
             {tasks.length > 0 && (
                 <>
+
                     <Buttons onClick={() => dispatch(toggleHideDone())} >
                         {hideDone ? "Pokaż" : "Ukryj"} ukończone
                     </Buttons>
                     <Buttons disabled={tasks.every(({ done }) => done)}
-                        onClick={()=> dispatch(setAllDone())}
+                        onClick={() => dispatch(setAllDone())}
                     >
                         Ukończ wszystkie
                     </Buttons>
