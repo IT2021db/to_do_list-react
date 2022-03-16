453d2359eeec9a3ae53fc17c784c34714d8981e9
Dorota Borowska <youcode2021db@gmail.com>
2022-03-16 22:53:32 +0100

    fetchExampleTasksSuccess instead setTasks

--- src/tasksSaga.js
+++ src/tasksSaga.js
@@ -1,21 +1,21 @@
 import { takeLatest, call, put, delay, takeEvery, select } from "redux-saga/effects";
 import { getExampleTasks } from "./features/tasks/getExampleTasks";
 import { saveTasksInLocalStorage } from "./features/tasks/tasksLocalStorage";
-import { fetchExampleTasks, selectTasks, setTasks } from "./features/tasks/tasksSlice";
+import { fetchExampleTasks, fetchExampleTasksSuccess, selectTasks, setTasks } from "./features/tasks/tasksSlice";
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
 
 export function* fetchExampleTasksHandler() {
     try {
         delay(1000);
         const exampleTasks = yield call(getExampleTasks);
-        yield put(setTasks(exampleTasks));
+        yield put(fetchExampleTasksSuccess(exampleTasks));
     } catch (error) {
         yield call(alert, "Coś poszło  nie tak!");
     }
-}
-
-function* saveTasksInLocalStorageHandler() {
-    const tasks = yield select(selectTasks);
-    yield call(saveTasksInLocalStorage, tasks);
 }
 
 export function* watchFetchExampleTasks() {
