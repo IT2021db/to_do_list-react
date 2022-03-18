5998004d2ee2c4358baa9631e5e704761515c569
Dorota Borowska <youcode2021db@gmail.com>
2022-03-18 11:56:46 +0100

    react-router-dom implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Link } from 'react-router-dom';
+import AuthorPage from './features/author/AuthorPage';
 
-const App = () => <Tasks />;
+const App = () => (
+	<HashRouter>
+		<nav>
+			<ul>
+				<li>
+					<Link to="/zadania">Zadania</Link>
+				</li>
+				<li>
+					<Link to="/author">O autorze</Link>
+				</li>
+			</ul>
+		</nav>
+		<Switch>
+			<Route path="/zadania">
+				<Tasks />
+			</Route>
+			<Route path="/author">
+				<AuthorPage />
+			</Route>
+		</Switch>
+	</HashRouter>
+)
 
 export default App;
