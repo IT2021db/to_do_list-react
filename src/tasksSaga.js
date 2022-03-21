da5a281d3e6ad2850809628a236eabf2f0db1956
Dorota Borowska <youcode2021db@gmail.com>
2022-03-21 22:09:56 +0100

    delay value changed

--- src/tasksSaga.js
+++ src/tasksSaga.js
@@ -1,7 +1,7 @@
 import { takeLatest, call, put, delay, takeEvery, select } from "redux-saga/effects";
 import { getExampleTasks } from "./features/tasks/getExampleTasks";
 import { saveTasksInLocalStorage } from "./features/tasks/tasksLocalStorage";
-import { fetchExampleTasks, fetchExampleTasksSuccess, selectTasks, setTasks } from "./features/tasks/tasksSlice";
+import { fetchExampleTasks, fetchExampleTasksSuccess, selectTasks } from "./features/tasks/tasksSlice";
 
 function* saveTasksInLocalStorageHandler() {
     const tasks = yield select(selectTasks);
@@ -10,7 +10,7 @@ function* saveTasksInLocalStorageHandler() {
 
 export function* fetchExampleTasksHandler() {
     try {
-        delay(1000);
+        delay(3000);
         const exampleTasks = yield call(getExampleTasks);
         yield put(fetchExampleTasksSuccess(exampleTasks));
     } catch (error) {
